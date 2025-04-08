7fe1778258c75cdb4a065ea707ed6c1c82f1752c
jiang_kui <jiangkui6@h-partners.com>
2025-04-08 16:01:41 +0800

    feat: 新增音频指纹、添加滤镜、视频添加字幕、图片合成视频能力
    Signed-off-by: jiang_kui <jiangkui6@h-partners.com>

--- harmony/ffmpeg_kit/src/main/ets/namespace/RNFFmpegKitModule.ts
+++ harmony/ffmpeg_kit/src/main/ets/namespace/RNFFmpegKitModule.ts
@@ -7,5 +7,8 @@ export namespace RNFFmpegKitModule {
     // 这里是定义arkts接口的文件，如果有需要，可在这里增加arkts接口
     addListener(eventName: string);
     removeListeners(count: number);
+    FFmpegKitCompleteCallbackEvent(sessionId: number);
+    FFmpegKitLogCallbackEvent(sessionId: number,level:number,message:String);
+    FFmpegKitStatisticsCallbackEvent(sessionId: number,videoFrameNumber:number,videoFps:number,videoQuality:number,size:number,time:number,bitrate:number,speed:number);
   }
 }
